fd2b894c0087323ee42a4d54068fa2f3dcf428ac
jeff-lewis <jlewis@streetconnect.com>
2016-06-22 20:04:13 -0400

    updated npm dependencies. tests failing

--- middleware.js
+++ middleware.js
@@ -3,11 +3,12 @@
 module.exports = function clsify(ns) {
   if (!ns) throw new Error('CLS namespace required');
 
-  return function (req, res, next) {
+  return function(req, res, next) {
     ns.bindEmitter(req);
     ns.bindEmitter(res);
 
-    ns.run(function () {
+    ns.run(function() {
+
       next();
     });
   };
